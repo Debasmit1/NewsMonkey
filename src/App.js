f0428194b39662f6915d5230bde6a1f2824e5a3b
DebasmitBiswal <debasmitbiswal@outlook.com>
2023-08-17 23:38:34 +0530

    Deleted unnecessary codes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import News from './components/News';
@@ -7,24 +6,12 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-// import LoadingBar from 'react-top-loading-bar'
-
-
 
 function App() {
-
-  // const [progress,setProgress] = useState(0)
-
-
   return (
     <div>
       <BrowserRouter>
         <Navbar/>
-        {/* <LoadingBar
-        color='#f11946'
-        progress={progress}
-        height= '3'
-      /> */}
         <Routes>
           <Route exact path="/" element={
            <News  pageSize={5} country="us" category="general"/>
